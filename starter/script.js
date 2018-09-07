f97a4e79957544c3a5425bf4dc21be9ef52e9a63
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-07 16:13:15 +0200

    Hoisting in practice

--- starter/script.js
+++ starter/script.js
@@ -1,21 +1,29 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+// functions
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+// retirement(1956);
+var retirement = function (year) {
+    console.log(65 - (2016 - year));
+}
+
+// Variables
+console.log(age);
+var age = 23;
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 ///////////////////////////////////////
 // Lecture: Scoping
